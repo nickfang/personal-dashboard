42e4bc97b64e77b7cb8cd8b7d4bfa165bec08977
Nick Fang <fang.nicholas@gmail.com>
2026-03-26 15:04:44 -0500

    Updating curl response format.

--- services/dashboard-api/internal/handlers/format.go
+++ services/dashboard-api/internal/handlers/format.go
@@ -13,7 +13,6 @@ func formatPressureText(pressureStats []*pressurePb.PressureStat) map[string]str
 	for _, pressureStat := range pressureStats {
 		location := pressureStat.LocationId
 		pressureByLocation[location] = ""
-		pressureByLocation[location] += fmt.Sprintf("%s\n", location)
 		pressureByLocation[location] += fmt.Sprintf("Pressure: %s\n", pressureStat.LastUpdated.AsTime().Local().Format("2006.01.02 15:04:05"))
 		pressureByLocation[location] += fmt.Sprintf("  %s\n", pressureStat.Trend)
 		pressureByLocation[location] += fmt.Sprintf("  Deltas: %.2f(1h), %.2f(3h), %.2f(6h) %.2f(12h) %.2f(24h)\n", pressureStat.Delta_1H, pressureStat.Delta_3H, pressureStat.Delta_6H, pressureStat.Delta_12H, pressureStat.Delta_24H)
@@ -26,36 +25,49 @@ func formatPollenText(pollenReports []*pollenPb.PollenReport) map[string]string 
 	for _, pollenReport := range pollenReports {
 		location := pollenReport.LocationId
 		pollenByLocation[location] = ""
-		pollenByLocation[location] += fmt.Sprintf("Plants: %s\n", pollenReport.CollectedAt.AsTime().Local().Format("2006.01.02 15:04:05"))
+		pollenByLocation[location] += fmt.Sprintf("Pollen: %s\n", pollenReport.CollectedAt.AsTime().Local().Format("2006.01.02 15:04:05"))
 		slices.SortFunc(pollenReport.Plants, func(a, b *pollenPb.PollenPlant) int {
-			if a.InSeason != b.InSeason {
-				if a.InSeason {
-					return -1
-				}
-				return 1
-			}
+			// uncomment to sort by In Season first
+			// if a.InSeason != b.InSeason {
+			// 	if a.InSeason {
+			// 		return -1
+			// 	}
+			// 	return 1
+			// }
 			return int(b.Index) - int(a.Index)
 		})
+		first := true
+		currentIndex := pollenReport.Plants[0].Index + 1
 		for _, plant := range pollenReport.Plants {
-			if plant.Index > 0 {
-				inSeason := "Out of Season"
-				if plant.InSeason {
-					inSeason = "In Season"
-				}
-				pollenByLocation[location] += fmt.Sprintf("  %s (%s): %d - %s\n", plant.DisplayName, plant.Category, plant.Index, inSeason)
-
+			if plant.Index < 1 {
+				break
 			}
-		}
-		pollenByLocation[location] += fmt.Sprintln("  Type:")
-		for _, pollenType := range pollenReport.Types {
-			if pollenType.Index > 0 {
-				inSeason := "In Season"
-				if pollenType.InSeason {
-					inSeason = "Out of Season"
+			if currentIndex != plant.Index {
+				if first {
+					first = false
+				} else {
+					pollenByLocation[location] += fmt.Sprintln()
 				}
-				pollenByLocation[location] += fmt.Sprintf("  %s: %s - %s\n", pollenType.Code, pollenType.Category, inSeason)
+				pollenByLocation[location] += fmt.Sprintf("  %-10s", plant.Category)
+				currentIndex = plant.Index
 			}
+			inSeason := "Out of Season"
+			if plant.InSeason {
+				inSeason = "In Season"
+			}
+			pollenByLocation[location] += fmt.Sprintf(" %s (%s)", plant.DisplayName, inSeason)
 		}
+		pollenByLocation[location] += fmt.Sprintln()
+		// pollenByLocation[location] += fmt.Sprintln("  Type:")
+		// for _, pollenType := range pollenReport.Types {
+		// 	if pollenType.Index > 0 {
+		// 		inSeason := "In Season"
+		// 		if pollenType.InSeason {
+		// 			inSeason = "Out of Season"
+		// 		}
+		// 		pollenByLocation[location] += fmt.Sprintf("  %s: %s - %s\n", pollenType.Code, pollenType.Category, inSeason)
+		// 	}
+		// }
 	}
 	return pollenByLocation
 }
@@ -72,8 +84,9 @@ func formatDashboardText(pressureStats []*pressurePb.PressureStat, pollenReports
 	}
 
 	data := ""
-	for _, location := range byLocation {
-		data += location
+	for location, info := range byLocation {
+		data += fmt.Sprintf("---------------- %s ----------------\n", location)
+		data += info
 	}
 	return data, nil
 }
